cf6d0524fd74ed1c5a55457735e690aea4e6c236
hakiimnurrizka <hakiimnur.2017@gmail.com>
2021-12-03 10:56:09 +0700

    Update analysis_paper2des.R

--- analysis_paper2des.R
+++ analysis_paper2des.R
@@ -37,3 +37,23 @@ summary(lm.3)
 par(mfrow = c(1,1))
 plot(lm.3)
 vif(lm.3)
+library(data.table)
+library(Publish)
+data("Diabetes")
+setDT(Diabetes)
+rm(Diabetes)
+setDT(data_paper2des)
+plot(1:49,sort(data_paper2des$int.score))
+data_paper2des[, int.group := cut(int.score,3,include.lowest=TRUE,
+                            labels=c("Rendah","Sedang","Tinggi"))]
+data_paper2des[,table(int.group)]
+data_paper2des[,letter.grade :=cut(grade,
+                        breaks=c(40,55,65,80,Inf),
+                        include.lowest=TRUE,
+                        labels=c("D","C","B","A"))]
+data_paper2des[,table(letter.grade)]
+boxplot(data_paper2des$posting ~ data_paper2des$int.group, xlab = "Tingkat keinginan", ylab =  "Posting")
+boxplot(data_paper2des$grade ~ data_paper2des$int.group, xlab = "Tingkat keinginan", ylab =  "Nilai akhir")
+table(data_paper2des$letter.grade, data_paper2des$int.group)
+data_paper2des[c(19,22,28,29,43),1:4]
+version
